78bec92f5d6024a6398494037ed15f60d717f5de
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-03-18 15:29:16 +0500

    Sort Header files and function to the header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import { useEffect, useState } from 'react';
-import "./App.css"
+import Main from './components/main/main'
+import "./style/general.scss";
+
 
 function App() {
 
-  const [data, setData] = useState({
-    loading: true,
-    error: null,
-    info: [],
-  })
-
-  useEffect(() => {
-
-    (async () => {
-      const dataFetched = await fetch("http://172.16.1.86:2001/upload");
-      console.log(dataFetched);
-      const json = await dataFetched.json()
-
-      setData({ loading: false, info: json, })
-    })()
-  }, []);
-
-
   return (
     <>
-      <main>
-        <div className='container'>
-          <ul>
-            {
-              data.info.map((item, i) => {
-                return (
-                  <li key={i}>{item.firstName}</li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </main>
+      <Main />
     </>
   );
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react';
+import "../../style/general.scss"
+import "./main.scss";
+
 function Main () {
 
-    return (
+    const [data, setData] = useState({
+        loading: true,
+        error: null,
+        info: [],
+      })
+    
+      useEffect(() => {
+    
+        (async () => {
+          const dataFetched = await fetch("http://172.16.1.86:2001/upload");
+          const json = await dataFetched.json()
+    
+          setData({ loading: false, info: json, })
+        })()
+      }, []);
+    
+    
+      return (
         <>
-            
+          <main>
+            <div className='container'>
+            <ul className='list'>
+                {
+                  data.info.map((item, i) => {
+                    return (
+                      <li key={i}>{item.firstName}</li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+          </main>
         </>
-    )
+      );
 
 }
 
