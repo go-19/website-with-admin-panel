9cf86ea6e94e06605a65ea77a13b0013c3056ddc
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-03-19 10:50:49 +0500

    Fix the bugs inside header file and reset

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,14 +7,21 @@ function Header() {
     <>
       <header>
         <div className="container site-header py-2">
-          <div>
+          <div className='w-25'>
             soat
           </div>
-          <a className='text-decoration-none d-flex flex-column align-items-center' href="index.js">
-            <span className='h2 logo py-2 pr-2'><span className='bg-primary text-white p-2'>Online</span> Rental Home</span>
+          <a className='w-50 text-decoration-none d-flex flex-column align-items-center' href="/">
+            <span className='h2 logo py-4 pr-2 mb-0'><span className='bg-primary text-white py-4 px-2'>Online</span> Rental Home</span>
           </a>
-          <div>
-            til, 
+          <div className='w-25 d-flex flex-column justify-content-start'>
+            <div className='ml-auto mb-auto'>
+              <button className='btn bg-primary text-white font-weight-bold mr-2'>SIGN IN</button>
+              <button className='btn bg-danger text-white font-weight-bold'>SIGN UP</button>
+            </div>
+            <div className='d-flex ml-auto'>
+              <span>til</span>
+              <span>tema</span>
+            </div>
           </div>
         </div>
       </header>
