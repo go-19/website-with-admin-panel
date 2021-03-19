9db0a4fd13268d0029c6b6665eca4dcd3b3e51ad
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-03-19 09:27:53 +0500

    Set site logo with png format

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,22 @@
 import "../../style/general.scss";
+import Logo from "../../assets/image/logo.png"
+import "./header.scss";
 
 function Header() {
 
   return (
     <>
       <header>
-        <div className="container">
-          
+        <div className="container site-header">
+          <div>
+            soat
+          </div>
+          <a href="index.js">
+            <img src={Logo} width="140" height="140" alt="logo"/>
+          </a>
+          <div>
+            til, tema, signin signup
+          </div>
         </div>
       </header>
     </>
