cb4a6b921ff600256d0b9d2da3e14c514454d57d
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-03-19 14:24:51 +0500

    Set date in real time and render it

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import "../../style/general.scss";
 import "./header.scss";
+import fullDate from './date';
 
 function Header() {
 
@@ -7,8 +8,8 @@ function Header() {
     <>
       <header>
         <div className="container site-header py-2">
-          <div className='w-25'>
-            soat
+          <div className='w-25 d-flex flex-column justify-content-end'>
+            <p className='h5 mb-0'>{fullDate.day}, {fullDate.gettingMonth()} {fullDate.monthDay}, {fullDate.year}</p>
           </div>
           <a className='w-50 text-decoration-none d-flex flex-column align-items-center' href="/">
             <span className='h2 logo py-4 pr-2 mb-0'><span className='bg-primary text-white py-4 px-2'>Online</span> Rental Home</span>
@@ -19,8 +20,14 @@ function Header() {
               <button className='btn bg-danger text-white font-weight-bold'>SIGN UP</button>
             </div>
             <div className='d-flex ml-auto'>
-              <span>til</span>
-              <span>tema</span>
+              <select className='p-2'>
+                <option value="uz">UZ</option>
+                <option value="ru">RU</option>
+                <option value="en">EN</option>
+              </select>
+              <button className='theme btn py-2'>
+                <i className='fas fa-moon moon'></i>
+              </button>
             </div>
           </div>
         </div>
