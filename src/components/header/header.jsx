4307d25c3107f3e820aefb8bf18a40b1fc5fb789
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-03-19 10:37:01 +0500

    set SIte logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,4 @@
 import "../../style/general.scss";
-import Logo from "../../assets/image/logo.png"
 import "./header.scss";
 
 function Header() {
@@ -7,15 +6,15 @@ function Header() {
   return (
     <>
       <header>
-        <div className="container site-header">
+        <div className="container site-header py-2">
           <div>
             soat
           </div>
-          <a href="index.js">
-            <img src={Logo} width="140" height="140" alt="logo"/>
+          <a className='text-decoration-none d-flex flex-column align-items-center' href="index.js">
+            <span className='h2 logo py-2 pr-2'><span className='bg-primary text-white p-2'>Online</span> Rental Home</span>
           </a>
           <div>
-            til, tema, signin signup
+            til, 
           </div>
         </div>
       </header>
